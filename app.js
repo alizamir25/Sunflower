5a548204be08449f22c2be661ac9735c33ada7c8
alizamir25 <ali0786zamir@gmail.com>
2024-09-04 13:33:20 +0100

    Patch

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const angleStep=137.5;
 const diamater=10;
 let angle=0, distance=diamater;
-function setup() {
+function setup(){
   createCanvas(windowWidth, windowHeight);
   background("#FBBC04");
   frameRate(10);
 }
-function draw() {
+function draw(){
   const p=new p5.Vector(cos(radians(angle))*distance, sin(radians(angle))*distance);
   push();
   translate(width/2, height/2);
@@ -16,7 +16,7 @@ function draw() {
   pop();
   angle += angleStep;
   distance+=diamater/720*angleStep;
-  if (p.x>width/2 || p.y>=height/2) { 
+  if (p.x>width/2 || p.y>=height/2){ 
     background("#FBBC04");
     angle=distance=diamater;
   }
